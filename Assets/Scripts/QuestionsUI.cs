cfc58a72c9bc6d8af23d86941e80f3efcbb354e3
DESKTOP-E4KCV2P\Hafsa Qamar <hafsaqamar9916@gmail.com>
2020-09-13 21:41:21 +0500

    6TH COMMIT - PROGRESSREPORT1

--- Assets/Scripts/QuestionsUI.cs
+++ Assets/Scripts/QuestionsUI.cs
@@ -10,11 +10,8 @@ public class QuestionsUI : MonoBehaviour
     public Transform buttonsContainer;
     public DataManager dataManager;
     public AudioSource audioSource;
-<<<<<<< HEAD
     public GameObject progressUI;
 
-=======
->>>>>>> 2db51f1c50b9ad0a5d45a8c097173f6955f72cd4
     private VPSProgress vpsProgress;
     // Start is called before the first frame update
 
@@ -22,32 +19,23 @@ public class QuestionsUI : MonoBehaviour
     {
         vpsProgress = new VPSProgress(); //object where we will store the progress
         var data = dataManager.vps.questions;
-        for (int i=0; i<data.Count; i++)
+        for (int i = 0; i < data.Count; i++)
         {
-            var buttonClone = Instantiate(buttonPrefab,buttonsContainer,false);
+            var buttonClone = Instantiate(buttonPrefab, buttonsContainer, false);
             var buttonScript = buttonClone.GetComponent<QuestionButton>();
             buttonScript.questionText.text = data[i].question;
             buttonScript.index = i;
             buttonScript.questionsUI = this;
-<<<<<<< HEAD
-            vpsProgress.progress.Add(new QsProgress() { questionId=i}); //
-=======
-            vpsProgress.progress.Add(new QsProgress() { questionId = i }); 
->>>>>>> 2db51f1c50b9ad0a5d45a8c097173f6955f72cd4
+            vpsProgress.progress.Add(new QsProgress() { questionId = i }); //
         }
         buttonPrefab.SetActive(false);
     }
     public void OnButtonEvent(int buttonID)
     {
-<<<<<<< HEAD
-        for (int i = 0; i < vpsProgress.progress.Count; i++) {
-            if (vpsProgress.progress[i].questionId == buttonID && dataManager.vps.questions[i].isCorrect) {
-=======
         for (int i = 0; i < vpsProgress.progress.Count; i++)
         {
-            if (vpsProgress.progress[i].questionId == buttonID)
+            if (vpsProgress.progress[i].questionId == buttonID && dataManager.vps.questions[i].isCorrect)
             {
->>>>>>> 2db51f1c50b9ad0a5d45a8c097173f6955f72cd4
                 vpsProgress.progress[i].attemptStatus = true;
                 vpsProgress.progress[i].marks += 1;
             }
@@ -56,16 +44,13 @@ public class QuestionsUI : MonoBehaviour
         audioSource.clip = dataManager.vps.questions[buttonID].answer;
         audioSource.Play();
         UnityEngine.Debug.Log(JsonUtility.ToJson(vpsProgress));
-<<<<<<< HEAD
         if (vpsProgress.Marks() == 5)
         {
             var clone = Instantiate(progressUI);
             clone.GetComponent<ProgressUI>().progressText.text = "marks obatained in history is " + 5;
         }
 
-=======
->>>>>>> 2db51f1c50b9ad0a5d45a8c097173f6955f72cd4
     }
 
-    
+
 }
