fa0cda857573e2a62d57ed2aafe01ba15a74411f
DESKTOP-E4KCV2P\Hafsa Qamar <hafsaqamar9916@gmail.com>
2020-09-23 14:56:46 +0500

    no message

---
+++ Assets/PulsePhysiologyEngine/Scripts/physicalexamreport/PhysicalExamButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class PhysicalExamButton : MonoBehaviour
+{
+    public ExamType examType;
+    public int index;
+    public int score;
+    public Action<int> callBack;
+    public void OnExamButtonClick() 
+    {
+        callBack?.Invoke(index);
+    }
+}
